bcf13511aed7241074918f64b4156aa5efcb0569
Juan Pedro Lozano <jplozano@yreaction.com>
2014-06-25 17:29:24 +0200

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -37,10 +37,10 @@ static const int GRID_COLUMNS = 10;
     float y = 0;
     
     _gridArray = [NSMutableArray array];
-    x = 0;
     for (int i = 0; i < GRID_ROWS; i++) {
         _gridArray[i] = [NSMutableArray array];
-        
+        x = 0;
+
         for (int j = 0; j < GRID_COLUMNS; j++) {
             Creature * creature = [[Creature alloc] initCreature];
             creature.anchorPoint = ccp(0,0);
