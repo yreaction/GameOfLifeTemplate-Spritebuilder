df0b03e75b75a01e8966f340be2580574044135f
Juan Pedro Lozano <jplozano@yreaction.com>
2014-06-25 17:39:15 +0200

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -68,8 +68,7 @@ static const int GRID_COLUMNS = 10;
     int colum = touched.x / _cellHeight;
     int row = touched.y / _cellWidth;
     NSLog(@"%i %i",row,colum);
-   // return _gridArray[row][colum];
-    return nil;
+    return _gridArray[row][colum];
 }
 
 @end
