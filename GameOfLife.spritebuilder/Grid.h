60e4d04aea89cdcbe3324f2ae5657608c3ad8718
Juan Pedro Lozano <jplozano@yreaction.com>
2014-06-25 17:01:58 +0200

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Juan Pedro Lozano on 25/06/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Juan Pedro Lozano on 25/06/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
